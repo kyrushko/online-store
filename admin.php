b7984b48c42be83e9990028174f2f04a6be52103
kyrushko <nikita.kyrushko@gmail.com>
2024-11-27 23:19:18 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -36,9 +36,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </div>
 </nav>';
 //Connect to MySQL
-        $host = "devweb2024.cis.strath.ac.uk";
-        $user = "qrb23133";
-        $pass = "aaS9heith6to";
+        $host = "";
+        $user = "";
+        $pass = "";
         $dbname = $user;
         $conn = new mysqli($host, $user, $pass, $dbname);
         $sql = "SELECT * FROM `Orders`";//note do not have `abc01234`. in front of the table name if you used $dbname to connect
